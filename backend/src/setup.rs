b7efaa58249e6100ff615f1e2609cbaee2ad71fe
Emil <e.roydev@gmail.com>
2025-02-10 23:47:46 +0200

    imporvements

--- backend/src/setup.rs
+++ backend/src/setup.rs
@@ -1,9 +1,13 @@
 use sea_orm::*;
+use std::env;
 
-const DATABASE_URL: &str = "postgresql://postgres:test123@localhost:5432/rocket_taskify";
+// const DATABASE_URL: &str = "postgresql://postgres:test123@localhost:5432/rocket_taskify"; // for local development
+
 
 pub async fn set_up_db() -> Result<DatabaseConnection, DbErr> {
-    let db = Database::connect(DATABASE_URL).await?;
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set"); // I load environment variable from render
+    
+    let db = Database::connect(&database_url).await?;
     println!("✅ Successfully connected to the database!");
 
     Ok(db)
